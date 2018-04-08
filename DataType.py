9d35a86b8a070f5b79ca2f5b9b602d23180bdd33
JACKSXPS\John <jpg267@msstate.edu>
2018-04-08 09:30:04 -0500

    Initial commit for DataType.py

---
+++ DataType.py
@@ -0,0 +1,11 @@
+#Data Types
+
+class DataTypes:
+
+	def __init__(self):
+		self.name = ""
+		self.sensitivity = None
+		self.frequencyOfAccess = None
+		self.reviewTime = None
+		self.Immutability = None
+		
